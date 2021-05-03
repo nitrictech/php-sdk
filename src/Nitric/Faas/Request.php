c1dd79568fa301201a3e470627597cdf5b759e8a
Tim Holm <tim.holm@nitric.io>
2021-05-03 16:57:50 +1000

    chore: Fix copyright headers

--- src/Nitric/Faas/Request.php
+++ src/Nitric/Faas/Request.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2021-2021 Nitric Pty Ltd.
+ * Copyright 2021 Nitric Technologies Pty Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 namespace Nitric\Faas;
 
 /**
