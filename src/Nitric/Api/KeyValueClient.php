fa1a0c8d40347f89db470da20ab00e8df1d47a49
Tim Holm <tim.holm@nitric.io>
2021-05-03 11:26:26 +1000

    chore: Commit linted files.

--- src/Nitric/Api/KeyValueClient.php
+++ src/Nitric/Api/KeyValueClient.php
@@ -1,6 +1,6 @@
 <?php
 
-/** 
+/**
  * Copyright 2021-2021 Nitric Pty Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 namespace Nitric\Api;
 
