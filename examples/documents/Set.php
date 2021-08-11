6e12c3a6ac677b70560c0a9a6ae3482a1b1be411
Ryan Cartwright <ryan.cartwright@nitric.io>
2021-08-11 10:32:23 +1000

    docs: Add initial examples.

---
+++ examples/documents/Set.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Copyright 2021 Nitric Technologies Pty Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START import]
+use Nitric\Api\Documents;
+// [END import]
+
+// [START snippet]
+$docs = new Documents();
+
+$document = $docs->collection("products")->doc("nitric");
+
+$document->set((object)[
+    "id" => "nitric",
+    "name" => "Nitric Framework",
+    "description" => "A development framework",
+]);
+// [END snippet]
+?>
